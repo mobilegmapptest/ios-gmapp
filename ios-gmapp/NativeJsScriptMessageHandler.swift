f1c79592eb393522d4f5107d6864b31e8a1f502c
Maitê Balhester <maitebalhester@gmail.com>
2017-03-24 19:03:05 -0300

    Refactoring ScriptMessageHandler

---
+++ ios-gmapp/NativeJsScriptMessageHandler.swift
@@ -0,0 +1,34 @@
+//
+//  NativeJsScriptMessageHandler.swift
+//  ios-gmapp
+//
+//  Created by Maite Balhester on 24/03/17.
+//  Copyright © 2017 Maite Balhester. All rights reserved.
+//
+
+import Foundation
+import WebKit
+
+class NativeJsScriptMessageHandler: NSObject, WKScriptMessageHandler {
+
+    var uiViewController: UIViewController!
+    
+    public init(uiViewController: UIViewController) {
+        self.uiViewController = uiViewController
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "messageBox" {
+            let sentData = message.body as! Dictionary<String, String>
+            
+            let message:String? = sentData["message"]
+            
+            let alertController = UIAlertController(title: nil, message: message, preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment:"btnOK"), style: .default, handler: nil))
+            uiViewController.present(alertController, animated: true) {}
+        }
+    }
+    
+    
+
+}
