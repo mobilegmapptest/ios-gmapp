942f1520b204e7f0bd49702904255f39e312cfeb
Maitê Balhester <maitebalhester@gmail.com>
2017-03-24 15:07:32 -0300

    Criação do projeto com webview

--- ios-gmapp/ViewController.swift
+++ ios-gmapp/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var webView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://google.com");
+        let requestObj = URLRequest(url: url!);
+        webView.loadRequest(requestObj);
     }
 
     override func didReceiveMemoryWarning() {
